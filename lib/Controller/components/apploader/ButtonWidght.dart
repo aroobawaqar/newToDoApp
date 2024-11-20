fb0838688dec5869f7b77e51beff793e4e7e0652
aroobawaqar <aroobawaqar.4414@gmail.com>
2024-11-20 18:36:51 +0500

    updated
    updated

---
+++ lib/Controller/components/apploader/ButtonWidght.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/Controller/colors/Appcolors.dart';
+class Buttonwidght extends StatelessWidget {
+  String text;
+  VoidCallback ontap;
+   Buttonwidght({super.key,
+     required this. text, required this.ontap
+   });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: ontap,
+      child: Container(
+        height: 40,
+          width: 200,
+        decoration: BoxDecoration(
+          color: AppColors.appPrimaryColor,
+          borderRadius: BorderRadius.circular(100)
+        ),
+        child: Center(child: Text(text,style: TextStyle(color: Colors.purple),),),
+      ),
+    );
+  }
+}
